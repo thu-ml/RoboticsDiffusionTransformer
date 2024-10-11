2901e752940a4136b35d7d981e7a707775f73607
Songming Liu <55993100+csuastt@users.noreply.github.com>
2024-10-11 20:05:09 +0800

    Update inference.sh

--- inference.sh
+++ inference.sh
@@ -1,5 +1,5 @@
 python -m scripts.agilex_inference \
     --use_actions_interpolation \
-    --pretrained_model_name_or_path="checkpoints/your_finetuned_ckpt.pt" \  # your finetuned checkpoint: e.g., checkpoints/your_finetuned_ckpt
+    --pretrained_model_name_or_path="checkpoints/your_finetuned_ckpt.pt" \  # your finetuned checkpoint: e.g., checkpoints/rdt-finetune-1b/checkpoint-<STEP NUMBER>, checkpoints/rdt-finetune-1b/checkpoint-<STEP NUMBER>/pytorch_model/mp_rank_00_model_states.pt,
     --lang_embeddings_path="outs/lang_embeddings/your_instr.pt" \
     --ctrl_freq=25    # your control frequency
