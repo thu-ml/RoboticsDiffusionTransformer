912d586b2bb58b1cc7517fcca1db771f9786453d
zachzhuu <yzhu714@connect.hkust-gz.edu.cn>
2025-04-23 13:02:01 +0800

    [Feat] HPC Platform Scripts
    - Refactor `finetune.sh` to meet the demand of HPC platform (just single A100 GPU now).

--- finetune.sh
+++ finetune.sh
@@ -1,3 +1,14 @@
+#!/bin/bash
+#SBATCH --job-name=rdt-finetune
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=16
+#SBATCH --gres=gpu:1
+#SBATCH --partition=i64m1tga800u
+#SBATCH --output=%j.out
+#SBATCH --error=%j.error
+
+# Multi-GPU related environment variables, unset them if you are not using multi-GPU
 # export NCCL_IB_HCA=mlx5_0:1,mlx5_1:1,mlx5_2:1,mlx5_3:1,mlx5_4:1,mlx5_7:1,mlx5_8:1,mlx5_9:1
 # export NCCL_IB_DISABLE=0
 # export NCCL_SOCKET_IFNAME=bond0
@@ -9,14 +20,24 @@ unset NCCL_SOCKET_IFNAME
 unset NCCL_DEBUG
 unset NCCL_NVLS_ENABLE
 
+# Activate your conda environment first
+source ~/.bashrc
+conda activate rdt
+
+module load cuda/12.2
+
+python -c "import torch; print(torch.cuda.device_count())"
+nvcc -V
+
+# Finetune the RDT model
 export TEXT_ENCODER_NAME="google/t5-v1_1-xxl"
 export VISION_ENCODER_NAME="google/siglip-so400m-patch14-384"
 export OUTPUT_DIR="./checkpoints/rdt-finetune-1b"
 export CFLAGS="-I/usr/include"
 export LDFLAGS="-L/usr/lib/x86_64-linux-gnu"
-export CUTLASS_PATH="/home/zach/repos/cutlass"
+export CUTLASS_PATH="/hpc2hdd/home/yzhu714/cutlass"
 
-export WANDB_PROJECT="robotics_diffusion_transformer"
+export WANDB_PROJECT="RDT_Finetune_HPC"
 
 if [ ! -d "$OUTPUT_DIR" ]; then
     mkdir "$OUTPUT_DIR"
@@ -30,13 +51,15 @@ fi
 #     --deepspeed="./configs/zero2.json" \
 #     ...
 
+wandb login ab70081f825fb1fa49d325cd15bee60b102f7176
+
 deepspeed --hostfile=hostfile.txt main.py \
     --deepspeed="./configs/zero2.json" \
     --pretrained_model_name_or_path="robotics-diffusion-transformer/rdt-1b" \
     --pretrained_text_encoder_name_or_path=$TEXT_ENCODER_NAME \
     --pretrained_vision_encoder_name_or_path=$VISION_ENCODER_NAME \
     --output_dir=$OUTPUT_DIR \
-    --train_batch_size=1 \
+    --train_batch_size=32 \
     --sample_batch_size=64 \
     --max_train_steps=200000 \
     --checkpointing_period=1000 \
